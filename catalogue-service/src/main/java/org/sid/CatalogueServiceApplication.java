367782f40659aef32dad064e3b7aaa8c2e438ce2
Ismail <salmi.ismail8u@gmail.com>
2019-02-17 16:04:02 +0100

    testing mongodb

--- catalogue-service/src/main/java/org/sid/CatalogueServiceApplication.java
+++ catalogue-service/src/main/java/org/sid/CatalogueServiceApplication.java
@@ -6,6 +6,7 @@ import java.util.stream.Stream;
 import org.sid.dao.CategoryRepository;
 import org.sid.dao.ProductRepository;
 import org.sid.entities.Category;
+import org.sid.entities.Product;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,13 +21,26 @@ public class CatalogueServiceApplication {
 	
 	@Bean
 	CommandLineRunner start(CategoryRepository categoryRepo , ProductRepository productRepo) {
-		categoryRepo.deleteAll();
 		return args->{
-			Stream.of("Ordinateur","Imprimantes")
+			categoryRepo.deleteAll();
+			Stream.of("C1 Ordinateur","C2 Imprimantes")
 			      .forEach(c->
-			      categoryRepo.save(new Category("C1",c,new ArrayList<>()))
+			      categoryRepo.save(new Category(c.split(" ")[0],c.split(" ")[1],new ArrayList<>()))
 			      );
 			categoryRepo.findAll().forEach(System.out::println);
+			Category c1 = categoryRepo.findById("C1").get();
+			productRepo.deleteAll();
+			Stream.of("P1","P2","P3","P4")
+		      .forEach(p->
+		      productRepo.save(new Product(null,p,Math.random()*1000,c1))
+		      );
+			Category c2 = categoryRepo.findById("C2").get();
+			Stream.of("P5","P6")
+		      .forEach(p->
+		      productRepo.save(new Product(null,p,Math.random()*1000,c2))
+		      );
+			productRepo.findAll().forEach(p->System.out.print(p.toString()));
+			
 		};
 	}
 
