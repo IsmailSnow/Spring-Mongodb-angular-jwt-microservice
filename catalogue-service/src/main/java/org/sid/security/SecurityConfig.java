7c49036d9bead2ffcf5704cc4afd5a3bc080bb7c
Ismail <salmi.ismail8u@gmail.com>
2019-02-24 16:12:36 +0100

    packaging apps and test ok

--- catalogue-service/src/main/java/org/sid/security/SecurityConfig.java
+++ catalogue-service/src/main/java/org/sid/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package org.sid.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -16,6 +17,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception{
 		http.csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().antMatchers(HttpMethod.GET ,"/categories/**").permitAll();
+		http.authorizeRequests().antMatchers(HttpMethod.GET ,"/products/**").permitAll();
         http.authorizeRequests().antMatchers("/categories/**").hasAnyAuthority("ADMIN");
         http.authorizeRequests().antMatchers("/products/**").hasAnyAuthority("USER");
         http.authorizeRequests().anyRequest().authenticated();
--- security-service/src/main/java/org/sid/security/SecurityParams.java
+++ security-service/src/main/java/org/sid/security/SecurityParams.java
@@ -3,6 +3,6 @@ package org.sid.security;
 public class SecurityParams {
 	public static final String JWT_HEADER_NAME="Authorization";
     public static final String SECRET="IsmailSnow@@";
-    public static final long EXPIRATION=10*24*3600;
+    public static final long EXPIRATION=10*24*3600*1000;
     public static final String HEADER_PREFIX="Bearer ";
 }
